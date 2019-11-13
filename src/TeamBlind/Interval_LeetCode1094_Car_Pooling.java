61ab6986677d01e077655f786cba775350d23e6e
Sabyasachi <mohantysabyasachi27@gmail.com>
2019-11-12 17:38:37 -0700

    String Questions - Leetcode 3

--- src/TeamBlind/Interval_LeetCode1094_Car_Pooling.java
+++ src/TeamBlind/Interval_LeetCode1094_Car_Pooling.java
@@ -6,7 +6,7 @@ public class Interval_LeetCode1094_Car_Pooling {
 
 	public static void main(String[] args) {
 		Interval_LeetCode1094_Car_Pooling ob = new Interval_LeetCode1094_Car_Pooling();
-		System.out.print(ob.carPooling(new int[][]{{2,1,5},{3,3,7}}, 5));
+		System.out.print(ob.carPooling(new int[][] { { 2, 1, 5 }, { 3, 3, 7 } }, 5));
 		System.out.println(ob.carPooling(new int[][] { { 3, 2, 7 }, { 3, 7, 9 }, { 8, 3, 9 } }, 11));
 	}
 
@@ -46,17 +46,18 @@ public class Interval_LeetCode1094_Car_Pooling {
 	public boolean carPooling(int[][] trips, int capacity) {
 
 		PriorityQueue<Event> pq = new PriorityQueue<>();
-		
-		for(int [] trip : trips) {
+
+		for (int[] trip : trips) {
 			pq.add(new Event(1, trip[1], trip[0]));
-			pq.add(new Event(-1, trip[2], -1*trip[0]));
+			pq.add(new Event(-1, trip[2], -1 * trip[0]));
 		}
-		
+
 		int capCheck = 0;
-		while(!pq.isEmpty()) {
+		while (!pq.isEmpty()) {
 			Event e = pq.poll();
 			capCheck += e.cap;
-			if(capCheck > capacity) return false;
+			if (capCheck > capacity)
+				return false;
 		}
 		return true;
 	}
---
+++ src/TeamBlind/String_LeetCode3_Longest_SubstringWithoutRepeatingChars.java
@@ -0,0 +1,54 @@
+package TeamBlind;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class String_LeetCode3_Longest_SubstringWithoutRepeatingChars {
+
+	public static void main(String[] args) {
+		String_LeetCode3_Longest_SubstringWithoutRepeatingChars ob = new String_LeetCode3_Longest_SubstringWithoutRepeatingChars();
+		// System.out.println(ob.lengthOfLongestSubstring("abcabcdecgha"));
+		// System.out.println(ob.lengthOfLongestSubstring("tmmzuxt"));
+		System.out.println(ob.lengthOfLongestSubstring("abcabcbb"));
+
+	}
+	/**
+	 * @author sunny
+	 * @param string
+	 * @return
+	 */
+
+	public int lengthOfLongestSubstring(String s) {
+		if (s == null || s.equals("")) {
+			return 0;
+		}
+		
+		if (s.length() == 1) {
+			return 1;
+		}
+
+		Map<Character, Integer> dict = new HashMap<>();
+		dict.put(s.charAt(0), new Integer(0));
+		int i = 0, len = 1, max = 1;
+		for (int j = 1; i < s.length() && j < s.length(); j++) {
+			char ch = s.charAt(j);
+			if (!dict.containsKey(ch)) {
+				dict.put(ch, j);
+				len++;
+				max = Math.max(len, max);
+			} else {
+
+				if (dict.get(ch) >= i) {
+					i = dict.get(ch) + 1;
+				}
+
+				len = j - i + 1;
+				max = Math.max(len, max);
+				dict.put(ch, j);
+
+			}
+		}
+		return max;
+	}
+
+}
